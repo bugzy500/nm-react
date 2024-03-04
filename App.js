f2f6571a53e53068ce221edf92b3a3fe4ecba7fb
bugzy500 <arpit96garg@outlook.com>
2024-03-05 01:16:07 +0530

    react init

---
+++ App.js
@@ -0,0 +1,3 @@
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const heading = React.createElement('h1',{},'Hello world') // (element-h1,div,p, props-basically attributes,children?)
+root.render(heading)
